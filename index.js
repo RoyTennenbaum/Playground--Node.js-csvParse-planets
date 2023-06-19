8afb8c5fd5f3b4798ed6c79baf0d379d19c03dc7
Roy Tennenbaum <roytenen10@gmail.com>
2023-06-19 14:44:24 +0300

    read and parse from csv file

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 import { parse } from "csv-parse";
 import { createReadStream } from "node:fs";
+
+const results = [];
+
+createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => console.log(err))
+  .on("end", () => {
+    console.log(`${results} DONE PROCCESSING FILE!`);
+  });
